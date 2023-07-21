0074555acb66d8f385564b6d51421ad672325f97
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-07-21 22:16:54 +0530

    added: FlatCards and ElevatedCard components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import FlatCards from './components/FlatCards'
+import ElevatedCards from './components/ElevatedCards'
 
 const App = () => {
   
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
+        <FlatCards />
+        <ElevatedCards />
       </ScrollView>
     </SafeAreaView>
   )
